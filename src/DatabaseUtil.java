64e25d0e47d59d00f862821ed1fc02d84c78a426
oussama lassoued <oussemalassoued601@hotmail.com>
2023-07-14 13:17:31 +0200

    add Database code

---
+++ src/DatabaseUtil.java
@@ -0,0 +1,13 @@
+package project_package;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class DatabaseUtil {
+    private static final String URL = "jdbc:mysql://localhost:3306/java_project";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "Java474@";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+}
